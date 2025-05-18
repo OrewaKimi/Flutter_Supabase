47bc10fb21548b6e4b55929e3dcdb03a6bdd3d21
OrewaKimi <kimi93.com@gmail.com>
2025-05-18 21:55:10 +0700

    update supabase setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,18 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_supabase/notes_page.dart';
+import 'notes_page.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-void main() {
+void main() async {
+// supabase setup
+WidgetsFlutterBinding.ensureInitialized();
+  await Supabase.initialize(
+  url:"https://vnsrqnmloqrtcabchhze.supabase.co",
+  anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZuc3Jxbm1sb3FydGNhYmNoaHplIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDc1Nzc5NDMsImV4cCI6MjA2MzE1Mzk0M30.toUyQIbK_zpHPu-YAElm1jgkHBM5xDZoS6HQo3N0uNc',
+  );
+  //
+
+// run the app
+
   runApp(const MyApp());
 }
 
