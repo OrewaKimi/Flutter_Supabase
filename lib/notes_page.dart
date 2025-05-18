acc4a97d4c8b469500f0574fc427ce598766f68c
OrewaKimi <kimi93.com@gmail.com>
2025-05-18 22:10:02 +0700

    create supabase

--- lib/notes_page.dart
+++ lib/notes_page.dart
@@ -8,8 +8,46 @@ class NotesPage extends StatefulWidget {
 }
 
 class _NotesPageState extends State<NotesPage> {
+  /*
+
+  CREATE - a note and save in supabase
+
+  */
+
+  final textController = TextEditingController();
+
+  void addNote() {
+    showDialog(
+      context: context,
+      builder:
+          (context) => AlertDialog(
+            content: TextField(controller: textController),
+            actions: [
+              // save button
+              TextButton(
+                onPressed: () {
+                  saveNote();
+                  Navigator.pop(context);
+                },
+                child: const Text('Save'),
+              ),
+            ],
+          ),
+    );
+  }
+
+  void saveNote() {
+    
+  }
+
+  /*
+   
+  READ - notes from supabase in app
+
+  */
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const Scaffold();
   }
 }
